cbbc72f304e5393a41f15f693a63c7beae5ba66b
Rosanna Giles <rosannagiles22@gmail.com>
2023-02-26 10:32:48 -0500

    update requirements

---
+++ queries.sql
@@ -0,0 +1,15 @@
+--List the employee number, last_name, first_name, sex and salary of each employee
+
+--List the first_name, last_name, and hire_date for the employees who were hired in 1986
+
+--List the manager of each department along with their department_number, department_name, employee_number, last_name, and first_name
+
+--List the department number for each employee along with that employee's employee_number, last_name, first_name and department_name
+
+--List first_name, last_name, and sex of each employee whose first_name is Hercules and wwhose last_name  begins with the letter "B"
+
+--List each employee in the sales department including their employee_number, last_name, first_name
+
+--List each employee in the Sales and Development departments, including their employee_number, last_name,first_name and department_name
+
+--List the frequency counts, in descending order, of all the employees last_names(that is , how many employess share each last_name)
